0e0de446c3b9bae979a4294c7cad827c80e6ca92
Lucia Llaneza <lu.llaneza@gmail.com>
2026-03-15 20:26:14 +0100

    docs:carpeta Kepler, actividad 1 y actividad 2

---
+++ 3_analisis_categorias.py
@@ -0,0 +1,10 @@
+prestamos = [
+    {"titulo": "1984", "categoria": "Ficción", "dias": 15},
+    {"titulo": "Sapiens", "categoria": "Ensayo", "dias": 22},
+    {"titulo": "Watchmen", "categoria": "Cómic", "dias": 18},
+    {"titulo": "El Quijote", "categoria": "Ficción", "dias": 30},
+    {"titulo": "Breve historia", "categoria": "Ensayo", "dias": 25},
+    {"titulo": "Batman", "categoria": "Cómic", "dias": 12}
+]
+
+print(prestamos[:1])
